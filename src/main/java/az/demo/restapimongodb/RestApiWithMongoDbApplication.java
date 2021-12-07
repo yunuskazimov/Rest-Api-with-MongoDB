acf66921a2f14ee5f8d936fbf57c37a5fae40432
Yunus Kazimov <77327532+yunuskazimov@users.noreply.github.com>
2021-12-07 15:59:30 +0400

    f com

---
+++ src/main/java/az/demo/restapimongodb/RestApiWithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package az.demo.restapimongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiWithMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/demo/restapimongodb/RestApiWithMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package az.demo.restapimongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
